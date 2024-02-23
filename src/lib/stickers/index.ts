7956076cd765a0a91fa9138143ffb541a3b0af4c
Jack Griffiths <jack.griffiths@outlook.com>
2024-02-23 14:07:05 +0000

    Move sticker images to lib folder so browsers cache them

--- src/lib/stickers/index.ts
+++ src/lib/stickers/index.ts
@@ -1,3 +1,26 @@
-export const getStickerImageUrl = (stickerTitle: string) => {
-  return `/stickers/${stickerTitle}.jpg`;
+// The sticker images are in the lib folder instead of the static
+// folder because assets in the static folder are not served by SvelteKit
+// with any cache headers, whereas assets in the lib folder are treated as
+// immutable. Lib folder assets get cache buster file names and have a
+// value of "immutable" in the Cache-Control header.
+
+// Because the sticker images don't have stable file names, their file
+// paths need to be imported (Vite takes care of resolving the dynamic
+// file path). It's not practical to import every sticker image
+// individually within Svelte components. Instead they are all imported
+// here accessed dynamically.
+
+const imageUrls = import.meta.glob(`/src/lib/images/stickers/*.jpg`, {
+  eager: true,
+  query: "?url",
+  import: "default",
+}) as Record<string, string>;
+
+export const getStickerImageUrl = (title: string) => {
+  // Single quotes have been removed from the image file names
+  // because Vite cannot build the application if there is an
+  // import statement for a file with a single quote.
+  const sanitizedTitle = title.replaceAll("'", "");
+  const sourceFilePath = `/src/lib/images/stickers/${sanitizedTitle}.jpg`;
+  return imageUrls[sourceFilePath] ?? "";
 }
