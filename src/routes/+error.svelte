b1379f7dbf852adecb28fa0fcf2869b263cd7d4d
Jack Griffiths <jack.griffiths@outlook.com>
2024-02-24 23:12:41 +0000

    Add padding to error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,15 +2,22 @@
   import { page } from "$app/stores";
 </script>
 
-<h1>Oops</h1>
-
-{#if $page.status === 404}
-  <p>That page doesn't exist. Try going back to the <a href="/">home</a> page.</p>
-{:else}
-  <p>Something went wrong. Please refresh the page and try again.</p>
-{/if}
+<div>
+  <h1>Oops</h1>
+  
+  {#if $page.status === 404}
+    <p>That page doesn't exist. Try going back to the <a href="/">home</a> page.</p>
+  {:else}
+    <p>Something went wrong. Please refresh the page and try again.</p>
+  {/if}
+</div>
 
 <style>
+  div {
+    padding-block: 2rem;
+    padding-inline: var(--page-gutter);
+  }
+
   h1 {
     font-size: 1.75rem;
     margin-block-end: 0.5em;
